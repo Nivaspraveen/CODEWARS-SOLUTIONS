1b2b08210421b2276317f634faa2c39b210d1634
Nivaspraveen <nivaspraveen07@gmail.com>
2024-11-18 20:45:52 +0530

    Added katas in 6kyu & 5kyu

---
+++ 6kyu/detect-pangram.js
@@ -0,0 +1,12 @@
+// Description:
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string){
+    const lowerCased = string.toLowerCase(), letterSet = new Set();
+    for (let char of lowerCased) {
+      if (char >= 'a' && char <= 'z') letterSet.add(char);
+    }
+    return letterSet.size === 26;
+}
