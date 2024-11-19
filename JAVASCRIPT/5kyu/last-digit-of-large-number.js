53cf488bfb3bfea60fd7930547784f9a5867abc9
Nivaspraveen <nivaspraveen07@gmail.com>
2024-11-19 13:21:48 +0530

    commented out examples

--- JAVASCRIPT/5kyu/last-digit-of-large-number.js
+++ JAVASCRIPT/5kyu/last-digit-of-large-number.js
@@ -60,11 +60,11 @@
 
 // You may assume that the input will always be valid.
 
-Examples
-lastDigit(4n, 1n)            // returns 4n
-lastDigit(4n, 2n)            // returns 6n
-lastDigit(9n, 7n)            // returns 9n  
-lastDigit(10n,10000000000n)  // returns 0n
+// Examples
+// lastDigit(4n, 1n)            // returns 4n
+// lastDigit(4n, 2n)            // returns 6n
+// lastDigit(9n, 7n)            // returns 9n  
+// lastDigit(10n,10000000000n)  // returns 0n
 
 function lastDigit(n, m) {  
     if (m === 0n) return 1n;
