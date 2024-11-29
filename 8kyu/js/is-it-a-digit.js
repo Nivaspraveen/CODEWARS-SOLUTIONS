1735e43486f968a98005dd69b3424fc05a6a77e0
Nivaspraveen <nivaspraveen07@gmail.com>
2024-11-29 19:17:22 +0530

    working on regex

---
+++ 8kyu/js/is-it-a-digit.js
@@ -0,0 +1,8 @@
+// Description 
+// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+
+// Regular Expressions, Fundamentals
+
+String.prototype.digit = function() {
+    return /^\d$/.test(this);
+};
